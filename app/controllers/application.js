6aabd62c29d0333e71eb68b96c38dd00671e219b
santuan <smrcouto88@gmail.com>
2016-09-27 19:52:54 -0300

    banderitas

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	isShowMenu: false,
-	oldClass: '',
 
-	init: function () {
-		this._super();
-	},
-	
-
-	getCurrentURL: function () {
-		this.set('isHome', false);
-		
-
-		if (RegExp('index').test(this.get('currentPath')) && !RegExp('reports').test(this.get('currentPath'))) {
-			this.set('isHome', true);
-		}
-	
-
-	}.observes('currentPath'),
 
 });
